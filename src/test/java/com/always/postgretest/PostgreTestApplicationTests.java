f9a32d76e7a540cee4a3526e5a3d0fbb23ded1fa
김한결 <khk12304@naver.com>
2023-12-07 18:11:39 +0900

    postgres 프로젝트 업로드

---
+++ src/test/java/com/always/postgretest/PostgreTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.always.postgretest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgreTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
